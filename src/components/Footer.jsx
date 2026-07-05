7059e548d7452f2f2dd02c536f5b8bbe765ef80c
MelAiia <shavlakayya@gmail.com>
2026-07-05 11:16:40 +0300

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+function Footer() {
+  return (
+    <footer>
+      This project was coded by{" "}
+      <a
+        href="https://www.linkedin.com/in/aiia-melnychenko-09b52a275/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Melnychenko Aiia
+      </a>{" "}
+      and is{" "}
+      <a
+        href="https://github.com/MelAiia/dictionary-react-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        open-sourced on GitHub
+      </a>{" "}
+      and is{" "}
+      <a
+        href="https://dictionary-react-aiia.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        hosted on Netlify
+      </a>
+      .
+    </footer>
+  );
+}
+
+export default Footer;
