044db0dd38f2d858757ebebc9fb602852a5bb7d9
MelAiia <shavlakayya@gmail.com>
2026-07-05 12:59:52 +0300

    SearchForm function

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -3,13 +3,24 @@ import { useState } from "react";
 function SearchForm() {
   const [word, setWord] = useState("");
 
+  function handleWordChange(event) {
+    setWord(event.target.value);
+  }
+
   return (
     <section className="SearchForm">
       <form>
-        <input type="search" placeholder="Enter a word..." autoFocus />
+        <input
+          type="search"
+          placeholder="Enter a word..."
+          autoFocus
+          onChange={handleWordChange}
+        />
 
         <button type="submit">Search</button>
       </form>
+
+      <p>Searching for: {word}</p>
 
       <small>Type a word and press Search to see its meaning.</small>
     </section>
