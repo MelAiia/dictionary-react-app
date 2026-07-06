464a4349ea478708efa7d42b90ab2a2d757583ac
MelAiia <shavlakayya@gmail.com>
2026-07-06 17:13:36 +0300

    Styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import "../styles/Header.css";
 function Header() {
   return (
     <header className="Header">
+      <div className="Header-icon">📖</div>
       <h1>Dictionary</h1>
       <p>Find definitions, pronunciations and examples.</p>
     </header>
