48f719686bb71c1790854dcef51643eb58069811
MelAiia <shavlakayya@gmail.com>
2026-07-06 15:43:52 +0300

    Added synonyms.jsx

--- src/components/Meaning.jsx
+++ src/components/Meaning.jsx
@@ -1,10 +1,14 @@
+import Synonyms from "./Synonyms";
+
 function Meaning({ meaning }) {
   const definition = meaning.definitions[0];
   return (
     <section className="Meaning">
       <h3>{meaning.partOfSpeech}</h3>
       <p>{definition.definition}</p>
-      {definition.example && <p className="example">"{definition.example}""</p>}
+      {definition.example && <p className="example">{definition.example}</p>}
+
+      <Synonyms synonyms={meaning.synonyms} />
     </section>
   );
 }
---
+++ src/components/Synonyms.jsx
@@ -0,0 +1,19 @@
+function Synonyms({ synonyms }) {
+  if (!synonyms || synonyms.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="Synonyms">
+      <h4>Synonyms</h4>
+
+      <ul>
+        {synonyms.map((synonym, index) => (
+          <li key={index}>{synonym}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Synonyms;
