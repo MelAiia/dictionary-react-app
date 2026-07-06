b0e5eb9f64883e6f04ee28949f3a526f7d9b6bda
MelAiia <shavlakayya@gmail.com>
2026-07-06 19:04:09 +0300

    Changed header structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
 import "../styles/Header.css";
-import SearchForm from "./SearchForm";
 import bookIcon from "../assets/icons/book.svg";
+import heroLeft from "../assets/illustrations/hero-left.svg";
+import heroRight from "../assets/illustrations/hero-right.svg";
+import heroDots from "../assets/illustrations/hero-dots.svg";
+
+import SearchForm from "./SearchForm";
 
 function Header({ setResult }) {
   return (
     <header className="Header">
-      <div className="Header-decoration left"></div>
+      <img src={heroLeft} alt="" className="hero-left" />
 
-      <div className="Header-decoration right"></div>
+      <img src={heroRight} alt="" className="hero-right" />
 
       <div className="Header-content">
         <div className="Header-icon">
           <img src={bookIcon} alt="Dictionary" />
         </div>
-
+        <img src={heroDots} alt="" className="hero-dots" />
         <h1>Dictionary</h1>
 
         <p>Find definitions, pronunciations and examples.</p>
