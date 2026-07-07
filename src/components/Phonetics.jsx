a05ecb9aab02e7d95bcff7e7293d070ecb85ba0f
MelAiia <shavlakayya@gmail.com>
2026-07-07 12:52:39 +0300

    Changed phonetics jsx and css

--- src/components/Phonetics.jsx
+++ src/components/Phonetics.jsx
@@ -1,14 +1,25 @@
+import speakerIcon from "../assets/icons/speaker.svg";
 import "../styles/Phonetics.css";
 
 function Phonetics({ phonetic }) {
+  function playAudio() {
+    const audio = new Audio(phonetic.audio);
+    audio.play();
+  }
+
   return (
     <div className="Phonetics">
-      <span>{phonetic.text}</span>
+      <div className="Phonetics-text">{phonetic.text}</div>
 
       {phonetic.audio && (
-        <a href={phonetic.audio} target="_blank" rel="noreferrer">
-          ▶ Listen
-        </a>
+        <button
+          type="button"
+          className="Phonetics-audio"
+          onClick={playAudio}
+          aria-label="Play pronunciation"
+        >
+          <img src={speakerIcon} alt="" />
+        </button>
       )}
     </div>
   );
